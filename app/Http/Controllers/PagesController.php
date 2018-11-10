56b93cbd2f9035295ae39027933841b6daa9aaaf
LuisG <luisegarzon@outlook.com>
2018-11-10 16:58:20 -0500

    bd

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,17 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function home()
-        {$messages = [
-            ['id'=> 1, 'content' => 'Este es mi primer mensaje!', 'image' => 'https://picsum.photos/600/338?image=0',],
-            ['id'=> 2, 'content' => 'Este es mi segundo mensaje!', 'image' => 'https://picsum.photos/600/338?image=1',],
-            ['id'=> 3, 'content' => 'Este es mi tercer mensaje!', 'image' => 'https://picsum.photos/600/338?image=2',],
-            ['id'=> 4, 'content' => 'Este es mi cuarto mensaje!', 'image' => 'https://picsum.photos/600/338?image=3',],
-        ];
+        {$messages = Message::all();
         return view('welcome',[
             'messages' => $messages,
             ]);    
